f9bfc740c996e15b58b45d0b53ed352b55bdedc6
Chanchal Mittal <86903677+chanchalmittal@users.noreply.github.com>
2021-11-06 22:49:01 +0530

    Create Q1.java

---
+++ solution/02-first-java.md/Q1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+//        Write a program to print whether a number is even or odd, also take input.
+
+
+        System.out.print("Enter the number: ");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+
+
+        if(number % 2 == 0){
+            System.out.println("Entered number is even");
+        }
+        else{
+            System.out.println("Entered number is odd");
+        }
+    }
+}
